a82949d2ace8e3bc8c9c43c684e7a22d9a231731
herasymj <jennifer.herasymuik@gmail.com>
2018-09-18 23:03:48 -0600

    Adding skeleton code to assi 1

---
+++ CS_350/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Assignment 1
+ * CS 350
+ * Jennifer Herasymuik
+ * 200352062
+ * 
+ * The purpose of this application is to implement the quicksort
+ *      both recursively and iteratively.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1
+{
+    class Program
+    {
+        /*
+         * Main functions that will run both sorts and return results to console.
+         * Will call both the iterative quick sort and the recursive one.
+         */
+        static void Main(string[] args)
+        {
+
+        }
+
+        /*
+         * recursiveQuickSort
+         * 
+         * Purpose: The prupose of this function is to implement the quicksort
+         *      algorithm recursively.
+         * Inputs:
+         *      list: an array of unsorted integers
+         * Returns: returns an integer array of sorted integers
+         */
+        int[] recursiveQuickSort(int[] list)
+        {
+
+        }
+
+        /*
+         * recursiveQuickSort
+         * 
+         * Purpose: The prupose of this function is to implement the quicksort
+         *      algorithm recursively.
+         * Inputs:
+         *      list: an array of unsorted integers
+         * Returns: returns an integer array of sorted integers
+         */
+        int[] iterativeQuickSort(int[] list)
+        {
+
+        }
+    }
+}
